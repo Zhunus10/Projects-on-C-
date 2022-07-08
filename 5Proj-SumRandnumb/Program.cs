cf016a1c562ee947bd7dca3f689088777f23c9b6
Zhunus <samsungus071@gmail.com>
2022-07-08 15:52:31 +0600

    add SumRandom

---
+++ 5Proj-SumRandnumb/Program.cs
@@ -0,0 +1,6 @@
+﻿int numbersA = new Random().Next(1, 1000); // 1 2 3 4 ...999
+Console.WriteLine(numbersA);
+int numbersB = new Random().Next(1000, 2000);
+Console.WriteLine(numbersB);
+int results = numbersA + numbersB;
+Console.WriteLine(results);
