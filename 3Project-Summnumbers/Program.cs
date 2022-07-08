ed189042257e5c7cc478e5d3adbc398d6e3d8b11
Zhunus <samsungus071@gmail.com>
2022-07-08 15:22:19 +0600

    add 3pro-Summ

---
+++ 3Project-Summnumbers/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numbersA = 15351;
+int numbersB = 251441;
+int result = numbersA + numbersB;
+Console.WriteLine(numbersA+numbersB);
