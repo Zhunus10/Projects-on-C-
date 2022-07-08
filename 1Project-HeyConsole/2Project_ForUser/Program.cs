635667de00133e4db96f1b64fe2c9c06d60636de
Zhunus <samsungus071@gmail.com>
2022-07-08 14:50:27 +0600

    add 2project

---
+++ 1Project-HeyConsole/2Project_ForUser/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Insert your name");
+string username = Console.ReadLine();
+Console.Write("Heyboy, ");
+Console.Write(username);
