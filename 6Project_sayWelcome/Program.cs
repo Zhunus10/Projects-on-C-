0c277f0b6bc322c57714f617b59ef26215856727
Zhunus <samsungus071@gmail.com>
2022-07-08 16:15:47 +0600

    add welcome files

---
+++ 6Project_sayWelcome/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insert user name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tanos")
+{
+    Console.WriteLine("Wellcome TANOS");    
+}
+else
+{
+    Console.Write("Hello Dear ");
+    Console.WriteLine(username);
+}
